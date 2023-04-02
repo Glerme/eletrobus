94d934e91f1ae8d2c8cf2498edac0b0fcf8750a2
Guilherme <guiggff@gmail.com>
2023-04-01 22:37:08 -0300

    :sparkles: First commit

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { NativeBaseProvider, Box } from "native-base";
+
+export default function App() {
+  return (
+    <NativeBaseProvider>
+      <Box>Hello world</Box>
+    </NativeBaseProvider>
+  );
+}
