afe8bf3ddba7dcf65e0f7862fb94cf193a7cd109
Guilherme <guiggff@gmail.com>
2023-04-03 14:41:46 -0300

    feat: testing apikey

--- App.tsx
+++ App.tsx
@@ -1,10 +1,43 @@
 import React from "react";
-import { NativeBaseProvider, Box } from "native-base";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import { StyleSheet, View } from "react-native";
 
 export default function App() {
   return (
-    <NativeBaseProvider>
-      <Box>Hello world</Box>
-    </NativeBaseProvider>
+    <>
+      <View style={styles.container}>
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          style={styles.map}
+          region={{
+            longitudeDelta: 0.01,
+            latitudeDelta: 0.0922,
+            latitude: -22.355457,
+            longitude: -48.776611,
+          }}
+          showsUserLocation
+          showsMyLocationButton
+          showsCompass
+          showsScale
+          showsTraffic
+          showsIndoors
+          showsBuildings
+          zoomControlEnabled
+          rotateEnabled
+          scrollEnabled
+          toolbarEnabled
+        />
+      </View>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+});
